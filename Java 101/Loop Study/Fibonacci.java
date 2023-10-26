eaf9e9d20f46770f18a1df05a9ba42f8b09fb962
Muhammed Berk DİNCER <m.berkdincer@hotmail.com>
2023-10-26 15:50:02 +0300

    Create Fibonacci.java

---
+++ Java 101/Loop Study/Fibonacci.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fibonacci {
+   public static void main(String[] args) {
+     List<Integer> fibo_List = new ArrayList<>();
+     fibo_List.add(0);
+     int i=0;
+     int y=1;
+      for(int n=0 ; n<8;n++){
+          int z=y+i;
+          fibo_List.add(z);
+          i=y;
+          y=z;
+
+        }
+
+     System.out.println(fibo_List);
+   }
+
+}
