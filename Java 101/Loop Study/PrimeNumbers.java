6c82de1659ed21b6975c1472acd533a440a93bf4
Muhammed Berk DİNCER <m.berkdincer@hotmail.com>
2023-10-26 15:23:12 +0300

    Update PrimeNumbers.java

--- Java 101/Loop Study/PrimeNumbers.java
+++ Java 101/Loop Study/PrimeNumbers.java
@@ -1,21 +1,24 @@
 import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class PrimeNumbers {
     public static void main(String[] args) {
-        List<Integer> primeList = new ArrayList<>();
-       
+        List<Integer> primeList = IntStream.rangeClosed(0, 100).boxed().collect(Collectors.toList());
+        
 
-        for(int i=1; i<=100;i++){
-            for(int y=1;y<10;y++){
+       for(int i=0; i<=98;i++){
+            for(int y=2;y<10;y++){
                if(i!=y && i%y==0){
-                   continue;
+                  primeList.remove(Integer.valueOf(i));
+                 continue;
                }
-                System.out.println(y);
             }
         }
 
-       // System.out.println(primeList);
+       System.out.println(primeList);
     }
 }
