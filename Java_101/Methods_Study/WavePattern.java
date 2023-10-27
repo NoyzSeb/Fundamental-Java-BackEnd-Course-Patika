7d0caabc4e74f8ba205985dee07fc1aebe175099
Muhammed Berk DİNCER <m.berkdincer@hotmail.com>
2023-10-27 14:33:07 +0300

    Update WavePattern.java

--- Java_101/Methods_Study/WavePattern.java
+++ Java_101/Methods_Study/WavePattern.java
@@ -1,7 +1,21 @@
 package Java_101.Methods_Study;
+import java.util.Scanner;
 
 public class WavePattern {
+    static void loop(int sayi) {
+        System.out.print(sayi + " ");
+        if (sayi > 0) {
+            loop(sayi - 5);
+        }
+        System.out.print(sayi + " ");
+    }
+
     public static void main(String[] args) {
-        
-    }   
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("N Number: ");
+        int n = scan.nextInt();
+        scan.close();
+        loop(n);
+    }
 }
