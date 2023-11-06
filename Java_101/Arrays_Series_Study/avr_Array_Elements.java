606066a5646247e7a282e9e8b9532723ae720fae
Muhammed Berk DİNCER <m.berkdincer@hotmail.com>
2023-11-06 13:48:04 +0300

    Create avr_Array_Elements.java

---
+++ Java_101/Arrays_Series_Study/avr_Array_Elements.java
@@ -0,0 +1,25 @@
+package Java_101.Arrays_Series_Study;
+import java.util.Scanner;
+
+public class avr_Array_Elements {
+    
+    static float harmonic_sum_calc(int n){
+        
+        if (n==1){
+            return 1;
+        }
+        
+        return harmonic_sum_calc(n-1)+((float)1/n);
+
+    }
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the length of the desired series");
+        int n= scan.nextInt();
+        scan.close();
+
+        System.out.println((n)/harmonic_sum_calc(n));
+
+    }
+}
